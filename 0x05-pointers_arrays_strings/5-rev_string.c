1eef065e0623adf3e7b6c6b6a2577e7914263f6c
clemjoseph <clemjoseph96@gmail.com>
2022-09-20 08:52:45 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,12 @@ void rev_string(char *s)
 {
 	int l = 0, i = 0;
 	char z;
-	
+
+
 	while (s[i++])
 		l++;
 
-	for (i = l - 1; i > l / 2; i--)
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		z = s[i];
 		s[i] = s[l - i - 1];
