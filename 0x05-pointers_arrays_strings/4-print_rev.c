51d9b06bd72b0fe8a82651d66f2af5d894108c3a
clemjoseph <clemjoseph96@gmail.com>
2022-09-20 09:04:01 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,14 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+	int i, n;
 
-	while s([i])
-		i++;
-	while (i--)
+	n = 0;
+	while s([n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
+	{
 		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
