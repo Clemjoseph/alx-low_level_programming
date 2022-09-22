f303e435075423146af2688d4fadf348c7613c9b
clemjoseph <clemjoseph96@gmail.com>
2022-09-21 18:19:34 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 	number = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		number = -n;
 	}
 	if (number / 10 != 0)
 	{
 		print_number(number / 10);
 	}
-	_putchar((number % 10) + '0');
+	putchar((number % 10) + '0');
 }
